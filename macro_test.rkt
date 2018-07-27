ea30831f935545f1729dffa79c617edfc36f5ae5
lackhoa <vodangkhoa873@gmail.com>
2018-07-27 17:25:41 +0300

    Good start with macro and testing (a pattern matching bug remaining)

---
+++ macro_test.rkt
@@ -0,0 +1,33 @@
+#lang racket
+
+(require rackunit
+         "macro.rkt")
+
+(check-equal? (case '(z)
+                [(tv i) "No!"]
+                [(S n) (+ n 5)]
+                [(z) "Yes!"])
+             "Yes!"
+             "Three patterns")
+
+
+(check-equal? (case (list 'foo 5 6)
+                [(z) "No!"]
+                [(foo a b) (* a b)])
+             30
+             "Using two variables")
+
+(def (max n m)
+  (case n
+    [(z)    m]
+    [(S n0) (case m
+              [(z)    (list 'S n0)]
+              [(S m0) (list 'S (max n0 m0))])]))
+
+(check-equal? (max (quote (S (z))) (quote (z)))
+             (quote (S (z)))
+             "Complex patterns 1")
+
+(check-equal? (max (quote (S (S (S (z))))) (quote (S (S (S (S (z)))))))
+             (quote (S (S (S (S (z))))))
+             "Complex pattern 2")
