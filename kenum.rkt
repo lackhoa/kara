68dfa3ac67b37fac03f08552d531e335c411616f
khoa <vodangkhoa873@gmail.com>
2018-08-09 20:31:19 +0300

    I think this is bad for my health

--- kenum.rkt
+++ kenum.rkt
@@ -1,38 +1,55 @@
 #lang racket
 (require "lang/kara.rkt")
 
-; Output: a list of expanded molecules, or INCONSISTENT error flag
-(def (expand mole)
-  (if (null? thunks)
-      'STABLE)
-    (let* ([thunks (mole 'thunks)]
-           [ft (car thunks)]
-           [path (thunk-path ft)]
-           [type (thunk-type ft)])
-      (if (path-in-molecule-containing-the-form)
-          (let ([ctor (mole-ref path)]
-            (if ctor-not-in-type
-                (error "ENUM" "Invalid constructor???" ctor)
-              (list (work-with-ctor mole ctor)))
-        ; Consider multiple possibilities
-        (map (lam (ctor)
-               (work-with-ctor (mole 'copy) ctor))
-             type))))))
+(struct Target path [type #:auto]
+  #:auto-value 'UNSPECIFIED)
+
+; Either kick-start or state the type of the target
+(def (kick-start mole)
+  (let* ([targets
+          (enqueue! (new-queue) (Target ""))])
+    (enum mole targets)))
+
+(def (enum mole targets)
+  (if (null? targets)
+      mole
+    (let* ([t-first (car targets)]
+           [t-rest  (cdr targets)]
+           [result (advance mole t-first)])
+      (if (eq? result 'INCONSISTENT)
+          null
+        (multitask
+          (for-each
+            (lam (result-iter)
+              (enqueue the new targets)
+              (enum result-mole targets))
+            result))))))
+
+; Output: a list of molecules, or INCONSISTENT error flag
+(def (advance mole target)
+  (if (target has type)
+      (map (lam (ctor)
+             (work-with-ctor (mole 'copy) ctor))
+           type-of-target)
+    (work-with-ctor mole (mole-ref target-path))))
 
 ; Will modify mole to expand a specific ctor
 (def (work-with-ctor mole ctor)
-  (call/cc (k)
-    (for-each
-      (lam (ctor-iter)
-        (match ctor-iter
-          [(form path constructor)
-           (when (eq? 'INCONSISTENT
-                      ((mole 'update) path constructor))  ; Returns false if the form is wrong
-                 (k 'INCONSISTENT))]
-          [(slink path1 path2)
-           (when (eq? 'INCONSISTENT
-                      ((mole 'add-slink) path1 path2))
-                 (K 'INCONSISTENT))]
-          [(rec name type)
-           add-more-thunks]))
-      ctor)))
+  (call/cc
+    (lam (k)
+      (for-each
+        (lam (ctor-iter)
+          (match ctor-iter
+            [(form path constructor)
+             (when (eq? 'INCONSISTENT
+                        ((mole 'update) path constructor))
+                   (k 'INCONSISTENT))]
+            [(slink path1 path2)
+             (when (eq? 'INCONSISTENT
+                        ((mole 'add-slink) path1 path2))
+                   (K 'INCONSISTENT))]
+            [(rec name type)
+             (unless (the path is in the slt)
+               (add that target somewhere to return later))]))
+        ctor)
+      (Return the molecule and the targets))))
