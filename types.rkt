89267e5612f06312686a51f64080cd030fa365b1
Dang Khoa Vo <vodangkhoa873@gmail.com>
2018-08-10 18:30:20 +0300

    Big improvement to types

--- types.rkt
+++ types.rkt
@@ -5,79 +5,139 @@
 (def sym->str symbol->string)
 
 ; -------------------------------------------------------
-; Some macros to start things off
+; Macros for Declaring Types
 ; -------------------------------------------------------
 ; Constructor declaration for components
+(struct Form (path ctor))
+
 (define-syntax-rule (form path e)
-  (tag 'form
-       (cons (sym->str 'path)
-             e)))
+  (Form (sym->str 'path) e))
 
 ; Symbolic links declaration
+(struct SLink (path1 path2))
+
 (define-syntax-rule (== path1 path2)
-  (tag 'slink
-       (cons (sym->str 'path1)
-             (sym->str 'path2))))
+  (SLink (sym->str 'path1) 
+         (sym->str 'path2)))
 
 ; Type declaration for components
+(struct Rec (component type))
+
 (define-syntax-rule (rec component type)
-  (tag 'rec
-       (cons (sym->str 'component)
-             type)))
+  (Rec (sym->str 'component)
+       type))
 
 ; Constructors
+(struct Ctor (name body))
+
 (define-syntax-rule (ctor name e ...)
-  (def name (list 'name e ...)))
+  (def name
+    (Ctor 'name (list e ...))))
 
 ; Types are delayed
+(struct Type (body))
+
 (define-syntax-rule (type name e ...)
-  (def name (delay (list e ...))))
+  (def name
+    (Type (delay (list e ...)))))
 
 ; Anonymous type
 (define-syntax-rule (union e ...)
-  (delay (list e ...)))
+  (Type (delay (list e ...))))
 
 ; -------------------------------------------------------
-; These are the types
+; Types
 ; -------------------------------------------------------
-(type wff A B C Implication)
+; Well-formed Formula
+(type wf A B C Implication)
 
 (ctor A) (ctor B) (ctor C)
 
 (ctor Implication
-  (rec ante wff)
-  (rec conclusion wff))
+  (rec ante wf)
+  (rec ccs wf))
 
-(type entailment I-Combinator
-                 K-Combinator
-                 Chain
+; Logical Entailment
+(type entailment AI AK AS AB AC
                  Modus-Ponens)
 
-(ctor I-Combinator
-  (form conclusion Implication)
-  (==   conclusion/ante conclusion/csq))
+; => A->A
+(ctor AI
+  (form ccs Implication)
 
-(ctor K-Combinator
-  (form conclusion      Implication)
-  (form conclusion/csq  Implication)
-  (==   conclusion/ante conclusion/csq/csq))
+  (== ccs/ante ccs/csq))
 
-(ctor Chain
-  (rec a=>b
-       (union I-Combinator K-Combinator))
-  (rec b=>c            entailment)
-  (rec conclusion      wff)
-  (== a=>b/conclusion
-      b=>c/prem))
+; => (A->B)->A
+(ctor AK
+  (form ccs     Implication)
+  (form ccs/csq Implication)
 
+  (== ccs/ante ccs/csq/csq))
+
+; => (A->(B->C)) -> ((A->B)->(A->C))
+(ctor AS
+  (form ccs          Implication)
+  (form ccs/ante     Implication)
+  (form ccs/csq      Implication)
+  (form ccs/ante/csq Implication)
+  (form ccs/csq/ante Implication)
+  (form ccs/csq/csq  Implication)
+  ; A
+  (== ccs/ante/ante
+      ccs/csq/ante/ante
+      ccs/csq/csq/ante)
+  ; B
+  (== ccs/ante/csq/ante
+      ccs/csq/ante/csq)
+  ; C
+  (== ccs/ante/csq/csq
+      ccs/csq/csq/csq))
+
+; => (B->C) -> ((A->B) -> (A->C))
+(ctor AB
+  (form ccs          Implication)
+  (form ccs/ante     Implication)
+  (form ccs/csq      Implication)
+  (form ccs/csq/ante Implication)
+  (form ccs/csq/csq  Implication)
+  ; A
+  (== ccs/csq/ante/ante
+      ccs/csq/csq/ante)
+  ; B
+  (== ccs/ante/ante
+      ccs/csq/ante/csq)
+  ; C
+  (== ccs/ante/csq
+      ccs/csq/csq/csq))
+
+; => (A->(B->C)) -> (B->(A->C))
+(ctor AC
+  (form ccs          Implication)
+  (form ccs/ante     Implication)
+  (form ccs/csq      Implication)
+  (form ccs/ante/csq Implication)
+  (form ccs/csq/csq  Implication)
+  ; A
+  (== ccs/ante/ante
+      ccs/csq/csq/ante)
+  ; B
+  (== ccs/ante/csq/ante
+      ccs/csq/ante)
+  ; C
+  (== ccs/ante/csq/csq
+      ccs/csq/csq/csq))
+
+; A, A->B => B
 (ctor Modus-Ponens
-  (rec  a->b       entailment)
-  (rec  a          entailment)
-  (rec  conclusion wff)
+  (rec ?=>a    entailment)
+  (rec ?=>a->b entailment)
 
-  (form a->b/conclusion Implication)
+  (form ?=>a->b/ccs
+        Implication)
 
-  (== a->b/conclusion/ante
-      a/conclusion)
-  (== conclusion
-      a->b/conclusion/csq))
+  (== ?=>a->b/ccs/ante
+      ?=>a/ccs)
+
+  (== ccs
+      ?=>a->b/ccs/csq))
+; Note: the conclusion (ccs) is implicit in the last link.
