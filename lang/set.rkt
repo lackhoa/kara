3a780693d5632a94837a4607b04f94a9edde1cb8
lackhoa <vodangkhoa873@gmail.com>
2018-08-30 18:56:10 +0300

    Separated the common library from the program, will break code

--- lang/set.rkt
+++
@@ -1,7 +0,0 @@
-#lang racket
-(require "macro.rkt"
-         "seq.rkt")
-(provide (all-defined-out))
-
-(def (set-filter pred set)
-  (list->set (lfilter pred (set->list set))))
--- lang/utils-test.rkt
+++
@@ -1,9 +0,0 @@
-#lang racket
-
-(require "macro.rkt"
-         "utils.rkt"
-         rackunit
-         racket/generator)
-
-(def g (generator () (yield 1) (yield 2) 3))
-(gen-get g 3)
