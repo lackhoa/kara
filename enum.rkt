377197cd0f8157a3ad29a4cd3dd9bc523e1ff72e
lackhoa <vodangkhoa873@gmail.com>
2018-09-20 00:16:32 +0300

    Just tested the new instance check (still need a ton more)

--- enum.rkt
+++ enum.rkt
@@ -5,33 +5,40 @@
 
 (provide (all-defined-out))
 
-(def (instance? r1 r2)
-  ;; Returns true iff r1 is an instance of r2.
+(def ctors
+  (let* ([ht (hash-set (hasheq) '-> 2)])
+    ht))
+
+(def (instance? ins model)
+  ;; Returns true iff ins is an instance of model.
   (def (same? root p1 p2)
     (or (member p1 (ref-sync root p2))
-       (and (eq? (ref-data root p1)
-               (ref-data root p2))
-          (eq? (length (ref-kids root p1))
-               (length (ref-kids root p2))
-               #|If this isn't true, the code below wouldn't work|#)
-          (for/and ([pk1 (kids-paths root p1)]
-                    [pk2 (kids-paths root p2)])
-            (same? root pk1 pk2)))))
+       (let ([ctor  (ref-data root p1)])
+         (match (ref-data root p2)
+           ['no-dat  #f]
+           [ctor     (and (eq? (length (ref-kids root p1))
+                             (hash-ref ctors ctor))
+                        (eq? (length (ref-kids root p2))
+                             (hash-ref ctors ctor))
+                        (for/and ([kp1  (kids-paths root p1)]
+                                  [kp2  (kids-paths root p2)])
+                          (same? root kp1 kp2)))]
+           [_        #f]))))
 
   (let loop ([path null])
-    (and (or (eq? (ref-data r2 path)
+    (and (or (eq? (ref-data model path)
                'no-dat)
-          (eq? (ref-data r1 path)
-               (ref-data r2 path)))
+          (eq? (ref-data ins path)
+               (ref-data model path)))
 
-       (andmap (lam (p)  (same? r1 path p))
-               (ref-sync r2 path))
+       (for/and ([p  (ref-sync model path)])
+         (same? ins path p))
 
-       (<= (length (ref-kids r2 path))
-          (length (ref-kids r1 path)))
+       (<= (length (ref-kids model path))
+          (length (ref-kids ins   path)))
 
        (andmap (lam (kid-path)  (loop kid-path))
-               (kids-paths r2 path)))))
+               (kids-paths model path)))))
 
 (def (make-mp fun arg)
   (pull (pull mp fun '[1]) arg '[2]))
--- test-enum.rkt
+++ test-enum.rkt
@@ -6,14 +6,37 @@
 
 (test-case
  "Generality"
- (check-eq? (instance? (new-root) (new-root))
+ (check-eq? (instance? (new-root)
+                       (new-root))
             #t)
- (check-eq? (instance? (new-root) (update (new-root) '[] '->))
+ (check-eq? (instance? (new-root)
+                       (update (new-root) '[] '->))
             #f)
  (check-eq? (instance? (update (new-root) '[] '->)
                        (update (new-root) '[] '->))
             #t)
- (check-eq? (instance? (update (update (new-root) '[0] '->)
-                               '[1] '->)
+ (check-eq? (instance? (sync (update (update (new-root) '[0] '->)
+                                     '[1] '->)
+                             '[0] '[1])
                        (sync (new-root) '[0] '[1]))
-            #t))
+            #t)
+ (let ([base (sync (sync (new-root) '[0] '[1]) '[2] '[3])])
+   (check-eq? (instance? base
+                         (sync base '[1] '[2]))
+              #f)))
+
+(test-case
+ "Generality Ultimate"
+ (def r1
+   ;; (-> A (-> B A))
+   (detach ak '[0]))
+
+ (def r2
+   ;; (-> (-> A B) (-> C (-> A B)))
+   (let ([up update] [sync sy])
+     (sy (sy (up (up (up (up (new-root) '[] '->) '[0] '->) '[1] '->) '[1 1] '->) '[0 0] '[1 1 0]) '[0 1] '[1 1 1])))
+
+ (check-eq? (instance? r2 r1)
+            #t)
+ (check-eq? (instance? r1 r2)
+            #f))
