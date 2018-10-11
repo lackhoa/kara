e64a64e8fb51dbe5985a50301441fe0c65a6ce53
lackhoa <vodangkhoa873@gmail.com>
2018-10-11 20:23:05 +0300

    Added command line argument to simplify batch jobs

--- main.rkt
+++ main.rkt
@@ -43,3 +43,38 @@
     (match (instance? thm (conclusion m))
       [#t  (dm m)]
       [#f  #f])))
+
+;;; Batch jobs
+(let ([flag  (command-line #:args ([flag "0"]) flag)])
+  (case flag
+    [("0")      (void)]
+
+    [("1" "2")  (let ([FILE-NAME  (match flag
+                                    ["1"  "db/data1.rkt"]
+                                    ["2"  "db/data2.rkt"])])
+                  (load FILE-NAME)
+                  (for ([i  (in-range 10)])
+                    (pydisplay "Cycle number:" i)
+                    (time (com 100))
+                    (time (col 1))
+                    (displayln (num))
+                    (save FILE-NAME)))]
+
+    [("3")      (let* ([db1        (file->list "db/data1.rkt")]
+                       [db2        (file->list "db/data2.rkt")]
+                       [merge      (append db1 db2)]
+                       [avg-len    (round (/ (length merge) 2))]
+                       [new-merge  (shuffle merge)])
+                  (let-values ([(new-db1 new-db2)
+                                (split-at new-merge avg-len)])
+                    (call-with-output-file "db/data1.rkt"
+                      #:exists 'truncate
+                      (lam (out)
+                        (for ([m  new-db1])
+                          (wm m out))))
+
+                    (call-with-output-file "db/data2.rkt"
+                      #:exists 'truncate
+                      (lam (out)
+                        (for ([m  new-db2])
+                          (wm m out))))))]))
